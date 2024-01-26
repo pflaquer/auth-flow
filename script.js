652846760b6d3d31d8fd9c254cf662a1e9efa193
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-01-26 18:05:31 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+const clientId = "YOUR_CLIENT_ID"; // Replace with your Google OAuth2 client ID
+const scope = "profile email"; // Adjust requested scope (e.g., "https://www.googleapis.com/auth/drive.readonly")
+
+function handleCredentialResponse(response) {
+  alert('logged in!');
+  console.log("Logged in!");
+  const accessToken = response.credential; // Access token for further API calls
+  // Get user information
+  fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${accessToken}`)
+    .then(response => response.json())
+    .then(data => {
+      const userInfo = `
+        <p>Welcome, ${data.name}!</p>
+        <p>Your email: ${data.email}</p>
+      `;
+      document.getElementById("user-info").innerHTML = userInfo;
+    });
+}
+
+function onSuccess(googleUser) {
+  const id_token = googleUser.getAuthResponse().id_token;
+  handleCredentialResponse({ credential: id_token });
+}
+
+function onFailure(error) {
+  console.error(error);
+}
+
+window.onload = function() {
+  gapi.load("auth2", function() {
+    gapi.auth2.init({
+      client_id: clientId,
+      scope: scope,
+    }).then(function(auth2) {
+      const button = document.getElementById("google-login-button");
+      button.addEventListener("click", function() {
+        auth2.signIn().then(onSuccess, onFailure);
+      });
+    });
+  });
+};
