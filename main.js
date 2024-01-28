c1914fab3dd7b5919d44f59999476e4cb2934989
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-01-28 13:42:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function onSignIn(googleUser) {
   
   alert('succesfully logged in!');
   gapi.auth2.init()
+  alert('auth2 initialized!');
   var auth2 = gapi.auth2.getAuthInstance();
   //var profile = googleUser.getBasicProfile();
   //console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
