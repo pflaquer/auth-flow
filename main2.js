15031987d0340458bebe69f6bc39a2a66c434aab
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-02-18 14:25:09 -0500

    Update main2.js

--- main2.js
+++ main2.js
@@ -182,6 +182,7 @@ document.querySelector("#navitem").forEach((x)=>{
 function busk(){
 	alert('working');
 	btns.classList.toggle('button-ani');
+	console.log('working...');
 }
 
 let btns = document.getElementById("pulse")
