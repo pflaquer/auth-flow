ad48bce80f1fe6f4bd2130ff9e8cbb98c289d544
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-02-28 08:04:53 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import App from "./index.js";
+import Index from "./index.js";
 import busk from "./functions.js";
 let render = (x)=>{
 
 	document.getElementById("root").innerHTML = x;
 }
 
-render(App());
+render(Index());
 
 let user;
 let events;
