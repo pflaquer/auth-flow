2a23abc20007a731225fb7d020bff0fdd421e4f4
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-02-28 12:49:58 -0500

    Update raw.js

--- src/raw.js
+++ src/raw.js
@@ -1,12 +1,3 @@
-import Index from "./index.js";
-import busk from "./functions.js";
-let render = (x)=>{
-
-	document.getElementById("root").innerHTML = x;
-}
-
-render(Index());
-
 let user;
 let events;
 let btns = document.getElementById("pulse");
