7c17e07dc1b3c42f30d5244a68f5fd316ef9221a
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-02-29 09:16:56 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -64,8 +64,9 @@ return (`
           <h1 class="title-page">Discover the best events!</h1>
         </div>
         <div class="page-content">
-        <div id="view">
+      
           <div class="box-map-event">
+            <div id="view">
             <div class="map" id="map">
              
               <div class="box-search">
