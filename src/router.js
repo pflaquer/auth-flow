a756509abd53856edc664a7dbff7bd6c27da7097
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-03-08 15:09:10 -0500

    Create router.js

---
+++ src/router.js
@@ -0,0 +1,23 @@
+/*
+let route;
+let routes = [home().view()]
+class home extends HTMLElement{
+constructor(view,title){
+this.view = view;
+this.title = title;
+}
+}
+
+function router(x)=>{
+return route[x].view();
+window.title = route[x].title;
+route = x;
+}
+
+
+const home = {
+view: ()=>{
+return 
+},
+title:null,
+*/
