cde40b8e9c8612c676c5a921f04f468677a7e3f3
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-07-18 10:23:42 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Index from "./index.js";
 import  Homie  from "../views/Home.js"
 import { busk, render, includeJS, loadView } from "./functions.js";
 
+if(authToken){
 render(Index());
+}
+else{
+return 'Not Authorized'
+}
 
 includeJS("/src/raw.js",'text/javascript');
