8a28d30e095039ebe256c282cd2bf7adc6b95a85
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-07-18 12:20:18 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Index from "./index.js";
 import Homie from "../views/Home.js"
 import NoAuth from "./noauth.js";
+import logout from './auth.js'
 
 import { busk, render, includeJS, loadView } from "./functions.js";
 
