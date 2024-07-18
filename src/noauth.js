c220c07cae361677b564807c47d0920d67f749a6
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-07-18 11:24:43 -0400

    Update noauth.js

--- src/noauth.js
+++ src/noauth.js
@@ -1,7 +1,11 @@
 function NoAuth(){
 
             return (`
+            <html>
+            <body style="background:white;">
             USER NOT AUTHORIZED
+            </body>
+            </html>
             `);
       }
 
