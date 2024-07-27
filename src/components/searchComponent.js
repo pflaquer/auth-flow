4475aa2c04c526898dc1d3a08f94095d90a183c2
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-07-27 17:41:03 -0400

    Create searchComponent.js

---
+++ src/components/searchComponent.js
@@ -0,0 +1,13 @@
+function searchComponent(){
+  
+  <div class='box'>
+<input type='text' value='' placeholder='Enter name, lastname or id'>
+<h1 class='info-search'>
+</h1>
+</div>
+<div class='search-box-result'>  </div>
+<template data-template-avatar>
+</template>
+}
+
+export default searchComponent
