1fb82a492e7ef471cf71b67388827d536aa9f013
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-07-28 13:57:20 -0400

    Update raw.js

--- src/raw.js
+++ src/raw.js
@@ -251,7 +251,7 @@ function Hire(){
 
 
 <div class='box'>
-<input type='text' value='' placeholder='Enter name, lastname or id' id="user-search" onclick="searchComponent()>
+<input type='text' value='' placeholder='Enter name, lastname or id' id="user-search" onclick="searchComponent()">
 <h1 class='info-search'>
 </h1>
 </div>
