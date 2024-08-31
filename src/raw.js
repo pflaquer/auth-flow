8a308dad775ae723aaf823b2ad2e585a3996d993
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-08-30 21:02:22 -0400

    Update raw.js

--- src/raw.js
+++ src/raw.js
@@ -20,10 +20,11 @@ events = [];
 //let custommarker = 'https://raw.githubusercontent.com/pflaquer/auth-flow/main/marker.png'
 //let custommarker = 'https://raw.githubusercontent.com/pflaquer/auth-flow/main/assets/mic.svg.png';
 //let custommarker = 'https://raw.githubusercontent.com/pflaquer/auth-flow/main/assets/mic2.svg.png';
+let altmarker = 'https://cdn-icons-png.flaticon.com/512/481/481546.png'
 let custommarker = altmarker;
 //let livemarker = 'https://raw.githubusercontent.com/pflaquer/auth-flow/main/marker.png';
 
-let altmarker = 'https://cdn-icons-png.flaticon.com/512/481/481546.png'
+
 let livemarker = altmarker;
 let venues = []
 let userimg = localStorage.getItem(1);
