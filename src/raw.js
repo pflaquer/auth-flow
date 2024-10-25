df6f4b776b055d4bf0b7b2bbd8d0fe233cc61f23
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-10-25 18:06:32 -0400

    Update raw.js

--- src/raw.js
+++ src/raw.js
@@ -15,7 +15,7 @@ function filterGenre(genre){
   */
 
 function buttonFilter(genre){
-	return `<div class="buttonFilter"><button>Filter</button> <select id="genres" name="genres" onchange='alert("Filter Applied!")+${markers[5]};console.log(markers)'>
+	return `<div class="buttonFilter"><button>Filter</button> <select id="genres" name="genres" onchange='alert("Filter Applied!");console.log(markers)'>
     <option value="Jazz">Jazz</option>
     <option value="HipHop">HipHop</option>
     <option value="Country">Country</option>
