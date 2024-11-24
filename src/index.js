6fc041c09ace62b30b9d9965850c794b4af28f48
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-11-24 11:10:20 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 //import app from './raw.js'
 //import logout from './auth.js'
-function submitRequest(){
-fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    title: 'foo',
-    body: 'bar',
-    userId: 1,
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((json) => alert(json));
 
-}
   
 function modalBox(){
 
