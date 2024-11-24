ce2b65576e8e69a85d4f44b473cf951d8570586f
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-11-24 11:13:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -122,7 +122,7 @@ return `
           </p>
         </div>
         <div class="modal-footer">
-          <button onclick="function(){
+          <button onclick="()=>{
           fetch('https://jsonplaceholder.typicode.com/posts', {
   method: 'POST',
   body: JSON.stringify({
