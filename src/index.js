91a318f692ebc0481af38a67a4ee46cbd3e4b601
Dromance <88290916+pflaquer@users.noreply.github.com>
2025-02-02 11:00:26 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -598,7 +598,7 @@ ${modalBox()}
                     </g>
                 </svg>
             </li>
-            <li>
+            <li onclick="fetch('https://go-vercel-teal.vercel.app/api/json').then(res=>res.json()).then(d=>console.log(d))">
                 <svg fill="#000000" viewBox="0 0 256 256" id="Flat" xmlns="http://www.w3.org/2000/svg">
                     <g id="SVGRepo_iconCarrier">
                         <path
