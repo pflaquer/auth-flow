95876c6d50f20b43797d1526fa69594368eb40e6
Dromance <88290916+pflaquer@users.noreply.github.com>
2025-04-17 14:57:51 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -698,7 +698,7 @@ ${modalBox()}
             </li>
             <!--<li onclick="testfetch()">-->
             <li onclick="logout()">
-                !--<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <!--<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g id="SVGRepo_iconCarrier">
                         <path d="M5 7H19" stroke="currentColor" stroke-width="3" stroke-linecap="round"
                             stroke-linejoin="round"></path>
