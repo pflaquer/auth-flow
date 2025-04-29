6bff7f5ad66a1d230697c19f391ffee10f41b6fa
Dromance <88290916+pflaquer@users.noreply.github.com>
2025-04-28 21:30:38 -0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+firebaseConfig = {
+      apiKey: btoa("AIzaSyCYFXrxtMjO2nlpsspIFxX1d2YovJJAHzg"),
+    authDomain: "buskitv2.firebaseapp.com",
+    projectId: "buskitv2",
+    storageBucket: "buskitv2.firebasestorage.app",
+    messagingSenderId: "317559494128",
+    appId: "1:317559494128:web:43ab1f124decf85782f935"
+    };
