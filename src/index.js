0bc401236c383e4455dcfcb837b3cc10e1e858a2
Dromance <88290916+pflaquer@users.noreply.github.com>
2025-05-29 01:12:37 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -488,7 +488,7 @@ function testfetch(){
 let JWT;
 JWT=1;
 let requests = 'Requests <span class="badge">NEW</span>'
-let newrequests=true;
+let newrequests=requestsState;
 /*let toggleState = (x)=>{
       x = true;
 }
