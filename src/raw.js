07a657df7ac28c2afb82a929b9004edd2de29180
Dromance <88290916+pflaquer@users.noreply.github.com>
2025-05-29 01:11:27 +0000

    Update raw.js

--- src/raw.js
+++ src/raw.js
@@ -7,7 +7,7 @@ let DEV_MODE;
 let routes =['Home'];
 let currentroute = routes[0]; 
 let globalMarkers = [];
-
+let requestsState=0;
 //new requests... user object has .requests with array number. if that number doesn't match the requests document number, then no new. if it does then new...
 /*
 db.collection(users).doc(UID).get()
